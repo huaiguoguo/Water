82a3b820eb29853b88a172b9c73af1bdb09b1a86
火柴 <crazywork@foxmail.com>
2020-10-30 00:16:07 +0800

    'water.keystore'

--- pages/device/test.vue
+++ pages/device/test.vue
@@ -1,12 +1,12 @@
 <template>
 	<view class="container">
-		<web-view src="http://localhost:3000"></web-view>
-		<!-- <view id="navmap" style="width: 100%; height: 300px;"></view> -->
+		<!-- <web-view src="http://localhost:3000"></web-view> -->
+		<view id="mapContainer" style="width: 100%; height: 300px;"></view>
 	</view>
 </template>
 
 <script>
-	// import AMapLoader from '@amap/amap-jsapi-loader';
+	import AMapLoader from '@amap/amap-jsapi-loader';
 	export default {
 		data() {
 			return {
@@ -42,23 +42,23 @@
 			console.log('-===============END');
 		},
 		onLoad() {
-			// this.getAMap();
+			this.getAMap();
 		},
 		methods: {
 			getAMap: function() {
 				AMapLoader.load({
 					key: "04af9fa6750c53530d457852d67b4656",
-					version: "1.4.15", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+					version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
 					plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
 					AMapUI: { // 是否加载 AMapUI，缺省不加载
 						version: '1.1', // AMapUI 缺省 1.1
 						plugins: [], // 需要加载的 AMapUI ui插件
 					},
-					Loca: { // 是否加载 Loca， 缺省不加载
-						version: '1.3.2' // Loca 版本，缺省 1.3.2
-					},
+					// Loca: { // 是否加载 Loca， 缺省不加载
+					// 	version: '1.3.2' // Loca 版本，缺省 1.3.2
+					// },
 				}).then((AMap) => {
-					this.map = new AMap.Map('navmap', {
+					this.map = new AMap.Map('mapContainer', {
 						resizeEnable: true,
 						center: [116.481181, 39.989792],
 						zoom: 16,
@@ -95,7 +95,7 @@
 
 
 				//鼠标点击marker弹出自定义的信息窗体
-				AMap.event.addListener(marker, 'click', function() {
+				map.event.addListener(marker, 'click', function() {
 					infoWindow.open(map, marker.getPosition());
 				});
 			},
@@ -148,7 +148,13 @@
 <style>
 	html,
 	body,
-	#container {
+	.container {
+		height: 100%;
+		width: 100%;
+	}
+	
+	
+	#mapContainer{
 		height: 100%;
 		width: 100%;
 	}
